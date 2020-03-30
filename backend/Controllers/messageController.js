08f278c041aa609da31a8d107d329e844af3c304
Bryce <bryce.tavares@gmail.com>
2020-03-30 17:02:31 +0200

    messageController.js

--- backend/Controllers/messageController.js
+++ backend/Controllers/messageController.js
@@ -1,3 +1,10 @@
 import mongoose from 'mongoose';
 import messageSchema from '../Models/messageModel';
 import userSchema from '../Models/userModel';
+
+const Message = mongoose.model('Message', messageSchema);
+const User = mongoose.model('User', userSchema);
+
+export const add = async (req, res) => {
+    let user = await User.findOne({ 'userName': req.body.userName });
+};
